0e1919ba0f4e343b81adabd135f163a5072c3184
vladislavsumin <vladislavsumin@mail.ru>
2022-02-02 22:06:15 +0300

    setup shared buildScript

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,11 @@
+import ru.vs.build_script.printHelloBuildScript
+
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+
+plugins {
+    id("empty_plugin")
+}
+
+printHelloBuildScript("dev-helper")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,12 @@
 rootProject.name = "dev-helper"
+pluginManagement {
+    includeBuild("../vs-core/buildScript")
 
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+}
+
+includeBuild("../vs-core")
